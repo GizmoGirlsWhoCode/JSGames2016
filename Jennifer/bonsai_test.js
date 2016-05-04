5a1847d370b3b3ae752c2f89ea50652cdac1ed57
Jennifer Hodgdon <yahgrp@poplarware.com>
2016-05-03 17:36:47 -0700

    More interesting bonsai project and project home page

--- Jennifer/bonsai_test.js
+++ Jennifer/bonsai_test.js
@@ -1,8 +1,32 @@
 // This is a test file for Bonsai.js.
 
-new Rect(100, 100, 100, 100).fill("red").addTo(stage);
+function increase_score(amt) {
+  if (typeof increase_score.score == 'undefined') {
+    increase_score.score = 0;
+    increase_score.text = new Text("").addTo(stage);
+  }
+  increase_score.score += amt;
+  stage.removeChild(increase_score.text);
+  increase_score.text = new Text("Your score is " + increase_score.score + ". Click the green rectangle to exit.").addTo(stage);
+}
 
-var score = 20;
+var redRect = bonsai.Path.rect(150, 150, 150, 150).attr({fillColor: 'red'});
+stage.addChild(redRect);
 
-new Text("Your score is " + score).addTo(stage);
+redRect.on('click', function(){
+    increase_score(1);
+});
 
+var blueRect = bonsai.Path.rect(250, 250, 250, 250).attr({fillColor: 'blue'});
+stage.addChild(blueRect);
+
+blueRect.on('click', function(){
+    increase_score(-1);
+});
+
+var greenRect = bonsai.Path.rect(1, 1, 100, 100).attr({fillColor: 'green'});
+stage.addChild(greenRect);
+
+greenRect.on('click', function(){
+    window.location.href = 'project.html';
+});
