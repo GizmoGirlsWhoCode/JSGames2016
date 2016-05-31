27b1a9201348d948797f6a4417d168fa7f4f48b9
Jennifer Hodgdon <yahgrp@poplarware.com>
2016-05-31 16:56:46 -0700

    Update Jessica scripts

--- Showcase/jessica_1.js
+++ Showcase/jessica_1.js
@@ -2,47 +2,53 @@ function doBackground(x, stage){
 
 var r = 0;
 var g = 0;
-var b = 240;
+var b = 250;
 
 var rect;
 var i;
 
-x = x % 720;
+x = x % 750;
 
-for (i = 0; i < 24; i++) {
+for (i = 0; i < 50; i++) {
   rect = new Rect(x, -50, 10, 850).fill("rgb(" + r + ',' + g + ',' + b + ')').addTo(stage);
-  b -= 10;
-  g += 10;
-  x += 10;
-  if (x > 720) {
+  b -= 5;
+  g += 5;
+  x += 5;
+  if (x > 750) {
     x = 0;
   }
   
   //rect.on('pointermove', clickResponse);
 }
+b = 0;
+g = 250;
 
-for (i = 0; i < 24; i++) {
+
+for (i = 0; i < 50; i++) {
   rect = new Rect(x, -50, 10, 850).fill("rgb(" + r + ',' + g + ',' + b + ')').addTo(stage);
-  g -= 10;
-  r += 10;
-  x += 10;
-  if (x > 720) {
+  g -= 5;
+  r += 5;
+  x += 5;
+  if (x > 750) {
     x = 0;
   }
   //rect.on('pointermove', clickResponse);
 }
+g = 0;
+r = 250;
 
-for (i = 0; i < 25; i++) {
+for (i = 0; i < 50; i++) {
   rect = new Rect(x, -50, 10, 850).fill("rgb(" + r + ',' + g + ',' + b + ')').addTo(stage);
-  r -= 10;
-  b += 10;
-  x += 10;
-  if (x > 720) {
+  r -= 5;
+  b += 5;
+  x += 5;
+  if (x > 750) {
     x = 0;
   }
   //rect.on('pointermove', clickResponse);
 }
-
+r = 0;
+b = 250;
 
 }
 
@@ -57,7 +63,7 @@ var masterX = 0;
 function timeoutFunc() {
   masterX += 10;
   doBackground(masterX, stage);
-  setTimeout(timeoutFunc,70);
+  setTimeout(timeoutFunc, 200);
 }
 
 timeoutFunc();
